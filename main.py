0b1fb0c64a6863eff978c6351015c578415e21c9
Eder Lima <devilsouls666@gmail.com>
2022-07-04 19:15:27 -0300

    docs: add comments

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask, request, render_template
 from uuid import uuid4 as v4
 
@@ -8,15 +7,22 @@ app.template_folder = "templates"
 
 @app.get("/")
 def root():
+    # Renders the "template/index.html" to a static page
+    # to be accessed over "http://localhost:5000/".
     return render_template("index.html"), 200
 
 
 @app.post("/upload")
 def receive_image():
+    # Route to receive an image file from the HTTP request.
     try:
+        # Gets the image file from the request form
+        # and aves it to the uploads folder locally.
         image_file = request.files["file"]
         image_file.save(f"uploads/{v4()}{image_file.filename}")
 
+        # Returns a JSON message indicating that the file
+        # was received successfully.
         return {
             "message": "Successfully uploaded file.",
         }, 200
@@ -29,4 +35,5 @@ def receive_image():
 
 
 if __name__ == "__main__":
+    # Access http://localhost:5000/ to get the index route.
     app.run(debug=True)
